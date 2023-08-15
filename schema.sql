05d8737dfbbe1fe9da40dacf7d79cb11a18e9b92
Anita Sharma <sharma.anita00001@gmail.com>
2023-08-15 14:36:36 +0545

    Create table for owners and species; add foreign keys for each

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-
+-- Table for animals
 CREATE TABLE animals (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
     name varchar(100), 
@@ -11,3 +11,30 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(20);
+
+-- Table for owners
+CREATE TABLE owners (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    full_name varchar(100), 
+    age int
+);
+
+-- Table for species
+CREATE TABLE species (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    name varchar(100)
+);
+
+-- Delete column species from animals table
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add columns species_id and owner_id to animals table
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD COLUMN owner_id INTEGER;
+
+-- Add foreign keys to animals table
+ALTER TABLE animals
+ADD FOREIGN KEY(species_id) REFERENCES species(id),
+ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
