d0cfcf6cf0b1740d15768c56e73311a4384447b0
Anita Sharma <sharma.anita00001@gmail.com>
2023-08-16 13:10:09 +0545

    create tables for vets, specializations and visits

--- schema.sql
+++ schema.sql
@@ -12,6 +12,9 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(20);
 
+------------------------------------------------------------
+------------------------------------------------------------
+
 -- Table for owners
 CREATE TABLE owners (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
@@ -38,3 +41,36 @@ ADD COLUMN owner_id INTEGER;
 ALTER TABLE animals
 ADD FOREIGN KEY(species_id) REFERENCES species(id),
 ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+--------------------------------------------------------------
+--------------------------------------------------------------
+
+-- Table for vets
+CREATE TABLE vets (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    name varchar(100),
+    age int,
+    date_of_graduation date
+);
+
+-- Table for specializations
+CREATE TABLE specializations (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    vets_id INTEGER,
+    species_id INTEGER,
+    FOREIGN KEY (vets_id) REFERENCES vets (id),
+    FOREIGN KEY (species_id) REFERENCES species (id)
+);
+
+-- Table for visits
+CREATE TABLE visits (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    animals_id INTEGER,
+    vets_id INTEGER,
+    date_of_visit date,
+    FOREIGN KEY (animals_id) REFERENCES animals (id),
+    FOREIGN KEY (vets_id) REFERENCES vets (id)
+);
+
+
+
